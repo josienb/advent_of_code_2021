d8daf507fee40d5176133b7555b106ed6eb5d0e2
Josien Braas <code@josienbraas.nl>
2021-12-03 13:11:52 +0100

    Add a solution for day 2, part 1

---
+++ day02/part1.rb
@@ -0,0 +1,16 @@
+input_file = File.join(File.dirname(__FILE__), 'part1_input')
+
+moves = {}
+moves['forward'] = []
+moves['up'] = []
+moves['down'] = []
+
+File.open(input_file).each do |m|
+  move = m.chomp.split
+  moves[move[0]] << move[1].to_i
+end
+
+final_horizontal_position = moves['forward'].sum
+final_depth = moves['down'].sum - moves['up'].sum
+
+puts "Final horizontal position multiplied by final depth is #{final_horizontal_position * final_depth}."
