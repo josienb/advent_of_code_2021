a8181a69896e68d828c17ae334bb5d81273feb5d
Josien Braas <code@josienbraas.nl>
2023-01-06 01:08:19 +0100

    Add a faster solution for day 5 (1)

--- day05/part1.rb
+++ day05/part1.rb
@@ -1,57 +1,29 @@
-class Line
-  attr_reader :all_points
+def horizontal_line?(point1, point2)
+  (point1[0] != point2[0]) && (point1[1] == point2[1])
+end
 
-  def initialize(point1, point2)
-    @point1 = point1
-    @point2 = point2
-    @all_points = all_non_diagonal_line_points
-  end
-
-  private
-
-  def all_non_diagonal_line_points
-    return [] if diagonal_line?
-
-    if horizontal_line?
-      points_on_x_line
-    else
-      points_on_y_line
-    end
-  end
-
-  def points_on_x_line
-    start_range = @point1[0] < @point2[0] ? @point1[0] : @point2[0]
-    end_range = start_range == @point1[0] ? @point2[0] : @point1[0]
-
-    (start_range..end_range).to_a.map { |r| [r, @point1[1]] }
-  end
-
-  def points_on_y_line
-    start_range = @point1[1] < @point2[1] ? @point1[1] : @point2[1]
-    end_range = start_range == @point1[1] ? @point2[1] : @point1[1]
-
-    (start_range..end_range).to_a.map { |r| [@point1[0], r] }
-  end
-
-  def diagonal_line?
-    @point1[0] != @point2[0] && @point1[1] != @point2[1]
-  end
-
-  def horizontal_line?
-    @point1[0] != @point2[0] && @point1[1] == @point2[1]
-  end
+def vertical_line?(point1, point2)
+  (point1[0] == point2[0]) && (point1[1] != point2[1])
 end
 
 input_file = File.join(File.dirname(__FILE__), 'input.txt')
 
-lines = []
+field = Array.new(1000) { Array.new(1000, 0) }
+
 File.open(input_file).each_line do |line|
   points = line.split(' -> ').map { |c| c.chomp.split(',').map(&:to_i) }
-  lines << Line.new(points[0], points[1])
+
+  if horizontal_line?(points[0], points[1])
+    y = points[0][1]
+    xs = points[0][0] > points[1][0] ? Range.new(points[1][0], points[0][0]).to_a : Range.new(points[0][0], points[1][0]).to_a
+    xs.map { |x| field[y][x] = field[y][x] + 1 }
+  elsif vertical_line?(points[0], points[1])
+    ys = points[0][1] > points[1][1] ? Range.new(points[1][1], points[0][1]).to_a : Range.new(points[0][1], points[1][1]).to_a
+    x = points[1][0]
+    ys.map { |y| field[y][x] = field[y][x] + 1 }
+  end
 end
 
-all_points = lines.map(&:all_points).flatten(1)
-points_with_overlap = all_points.select { |c| all_points.count(c) > 1 }
-number_of_points_with_overlap = points_with_overlap.uniq.count
+number_of_points_with_overlap = field.flatten.count { |value| value > 1 }
 
 puts "The number of points where at least two lines overlap is #{number_of_points_with_overlap}."
